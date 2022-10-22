888163aedba86d7ae351cbb7c7156c4ab0ee2357
willem <willem@wh.com>
2022-10-23 01:00:33 +0800

    autostart

--- config.def.h
+++ config.def.h
@@ -9,7 +9,7 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag     = True;     /* Switch view on tag switch*/
 static const char *fonts[]          = { "monospace:size=18", 
 					"WenQuanYi Micro Hei:size=18:style=regular:antialias=true:autohint=true", 
-					"Symbols Nerd Font:pixelsize=22:style=2048-em:antialias=true:autohint=true" }; 
+					"Symbols Nerd Font:pixelsize=26:style=2048-em:antialias=true:autohint=true" }; 
 static const char dmenufont[]       = "monospace:size=18"; 
 static const char col_gray1[]       = "#665879";
 static const char col_gray2[]       = "#93859f";
